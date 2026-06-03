c7a78d069572682b1f15ef6343b910d284550449
hari <hariprasad8976@gmail.com>
2026-06-03 10:23:56 +0530

    added location icon on heaDER

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { LocationService } from '../../shared/location.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTooltipModule,
+  ],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  private locationService = inject(LocationService);
+
+  userLocationName  = this.locationService.userLocationName$;
+  userLocationCords = this.locationService.userLocationCords$;
+
+  retryLocation(): void {
+    this.locationService.detectLocation();
+  }
+}
