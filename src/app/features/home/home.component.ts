57cdf2eaf4f279c25178171ced6a913a5d815da3
hari <hariprasad8976@gmail.com>
2026-06-05 12:39:22 +0530

    test

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -5,7 +5,7 @@ import { MatExpansionModule } from '@angular/material/expansion';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { GroupListRequestBackend } from './home.models';
+import { GroupListApiResponse, GroupListRequestBackend, GroupListResponse } from './home.models';
 import { HomeService } from './home.service';
 import { HeaderService } from '../../components/header/header.service';
 import { NotifierService } from '../../shared/notifier/notifier.service';
@@ -32,6 +32,7 @@ export class HomeComponent {
   userLocationCords = this.headerService.userLocationCords;
   radiusOptions: number[] = [1, 5, 10, 25, 100, 1000];
   selectedRadius: number = 5;
+  groupList: GroupListResponse[] = [];
 
   constructor() {
     // Watch for changes in user location and fetch groups when available
@@ -55,17 +56,17 @@ export class HomeComponent {
       // Don't fetch if location is not available yet
       return;
     }
-    
+
     // Send flat lat/long format as expected by backend
     const body: GroupListRequestBackend = {
       lat: locationCoords.lat,
       long: locationCoords.long,
       rangeKm: this.selectedRadius,
     };
-    
+
     this.homeService.getGroupListByRange(body).subscribe({
-      next: (data) => {
-        console.log('Groups fetched:', data);
+      next: (data: GroupListApiResponse) => {
+        this.groupList = data.groups;
       },
       error: (error) => {
         this.notifier.error(error?.error?.message || error?.error || 'Failed to fetch groups');
