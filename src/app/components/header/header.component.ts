8ecfa5496ac7d453f7f63f63fc0d42f6df01f162
hari <hariprasad8976@gmail.com>
2026-06-05 19:26:35 +0530

    design

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, signal, inject } from '@angular/core';
+import { Component, OnInit, signal, inject, ViewChild, ElementRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
@@ -7,6 +7,7 @@ import { MatTooltipModule } from '@angular/material/tooltip';
 import { HeaderService } from './header.service';
 import { LocationCoords } from './header.models';
 import { NotifierService } from '../../shared/notifier/notifier.service';
+
 @Component({
   selector: 'app-header',
   standalone: true,
@@ -24,6 +25,10 @@ export class HeaderComponent implements OnInit {
 
   locationName = signal<string>('Locating...');
   isLoading = signal<boolean>(true);
+  isSearchFocused = signal<boolean>(false);
+  hasSearchText = signal<boolean>(false);
+
+  @ViewChild('pillInput') pillInput!: ElementRef<HTMLInputElement>;
 
   private readonly notifier = inject(NotifierService);
 
@@ -36,12 +41,12 @@ export class HeaderComponent implements OnInit {
   detectLocation() {
     this.isLoading.set(true);
     this.locationName.set('Locating...');
-    
-if (navigator.geolocation) {
+
+    if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         (position) => {
           console.log('User location:', position.coords);
-          const body: LocationCoords= {
+          const body: LocationCoords = {
             lat: position.coords.latitude,
             long: position.coords.longitude
           };
@@ -75,5 +80,25 @@ if (navigator.geolocation) {
     }
   }
 
+  onSearchFocus() {
+    this.isSearchFocused.set(true);
+  }
+
+  onSearchBlur() {
+    this.isSearchFocused.set(false);
+  }
+
+  onSearchInput(event: Event) {
+    const value = (event.target as HTMLInputElement).value;
+    this.hasSearchText.set(value.length > 0);
+  }
+
+  clearSearch() {
+    if (this.pillInput?.nativeElement) {
+      this.pillInput.nativeElement.value = '';
+      this.hasSearchText.set(false);
+      this.pillInput.nativeElement.focus();
+    }
+  }
 
 }
